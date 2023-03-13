74b292b70c49c4bf380c8ef5db435c992fb52109
hamburger1993 <philipp.brzosa@gmx.de>
2023-03-13 09:52:38 +0100

    Resolved Saša suggestions

--- rules-dev/transpile.py
+++ rules-dev/transpile.py
@@ -39,41 +39,51 @@ for dirpath, dirnames, filenames in os.walk(folder_path):
         # Check if it's a .ps1 file and not in an excluded folder
         if filename.endswith('.ps1') and not any(exclude_folder in file_path for exclude_folder in exclude_folders):
             print("Processing: ", file_path )
-            with open(file_path, 'r') as file:
-                # Read the contents of the file
-                content = file.read()
-    
-                # Search for includes
-                includes = re.findall(r'(?<=Shared\\)[^\\]+\.ps1', content, flags=re.IGNORECASE)
-                
-                print("\tFound: ", len(includes), " include(s)")
-    
-                # Loop through all found includes
-                for include in includes:
-                    # Replace the include with the contents of the corresponding file
-                    include_path = os.path.join(include_folder, include)
-                    with open(include_path, 'r') as include_file:
-                        include_content = include_file.read()
-                        includes_ex = re.findall(r'.*(?<=Shared\\)' + re.escape(include) + r'.*', content, flags=re.IGNORECASE)
-                        
-                        if len(includes_ex) > 0:
-                            content = content.replace(includes_ex[0], include_content)
-                            
-                        content = re.sub(r'.*#Requires -Version.*(\r?\n)?', '', content, flags=re.IGNORECASE)
-    
-                # Extract subfolder name
-                subfolder = os.path.relpath(os.path.dirname(file_path), folder_path)
-                
-                # Write the transpiled contents to a new file in the "transpiled" directory
-                transpiled_path = os.path.join(output_folder, subfolder, filename)
-                transpiled_path_dir = os.path.join(output_folder, subfolder)
-                
-                # Create subfolders if missing
-                if not os.path.exists(transpiled_path_dir):
-                    os.makedirs(transpiled_path_dir)
+
+            # Handle exceptions when opening the file
+            try:
+                # Open the file
+                with open(file_path, 'r') as file:
+                    # Read the contents of the file and check if the content was read successfully
+                    content = file.read()
+
+                    if not content:
+                        print("\tError: Could not read file: ", file_path)
+                        continue
+        
+                    # Search for includes
+                    includes = re.findall(r'(?<=Shared\\)[^\\]+\.ps1', content, flags=re.IGNORECASE)
                     
-                print("\tWriting file: ", transpiled_path)
-                with open(transpiled_path, 'w') as transpiled_file:
-                    transpiled_file.write(content)
+                    print("\tFound: ", len(includes), " include(s)")
+        
+                    # Loop through all found includes
+                    for include in includes:
+                        # Replace the include with the contents of the corresponding file
+                        include_path = os.path.join(include_folder, include)
+                        with open(include_path, 'r') as include_file:
+                            include_content = include_file.read()
+                            includes_ex = re.findall(r'.*(?<=Shared\\)' + re.escape(include) + r'.*', content, flags=re.IGNORECASE)
+                            
+                            if len(includes_ex) > 0:
+                                content = content.replace(includes_ex[0], include_content)
+                                
+                            content = re.sub(r'.*#Requires -Version.*(\r?\n)?', '', content, flags=re.IGNORECASE)
+        
+                    # Extract subfolder name
+                    subfolder = os.path.relpath(os.path.dirname(file_path), folder_path)
+                    
+                    # Write the transpiled contents to a new file in the "transpiled" directory
+                    transpiled_path = os.path.join(output_folder, subfolder, filename)
+                    transpiled_path_dir = os.path.join(output_folder, subfolder)
+                    
+                    # Create subfolders if missing
+                    if not os.path.exists(transpiled_path_dir):
+                        os.makedirs(transpiled_path_dir)
+                        
+                    print("\tWriting file: ", transpiled_path)
+                    with open(transpiled_path, 'w') as transpiled_file:
+                        transpiled_file.write(content)
+            except:
+                print("\tError: Could not open file: ", file_path)
         else:
             print("Skipping: ", file_path)
