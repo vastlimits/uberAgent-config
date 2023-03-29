2a30eb8c8fca2928474f1ac0f1ab14e18c3d77df
hamburger1993 <philipp.brzosa@gmx.de>
2023-03-29 12:26:58 +0200

    Added dynamic branc specific badge generation

--- config-dev/updateBadge.py
+++ config-dev/updateBadge.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 try:
     # Path to the folder to search
@@ -9,8 +10,14 @@ except:
     # Exit the script
     exit()
 
-read_me_md = os.path.join(working_dir, 'README.md')
+file_read_me = os.path.join(working_dir, 'README.md')
+badge_list = []
 
+def add_custom_badge(tag, description, value, color):
+    badge_list.append(f"![{tag}](https://img.shields.io/badge/{description}-{value}-{color})")
+
+def add_license_badge():
+    badge_list.append("![license](https://img.shields.io/github/license/vastlimits/uberAgent-config)")
 
 def get_current_branch():
     try:
@@ -23,3 +30,35 @@ def get_current_branch():
 
 branch = get_current_branch()
 print(f"Generating badge for branch: {branch}")
+
+ # Handle exceptions when opening the file
+try:
+    # Open the file
+    with open(file_read_me, 'r') as read_file:
+        # Read the file
+        file_content = read_file.read()
+        read_file.close()
+
+        pattern = r'\[comment\]: # \(BADGE_SECTION_START\)(.*?)\[comment\]: # \(BADGE_SECTION_END\)'
+        match = re.search(pattern, file_content, flags=re.DOTALL)
+
+        if match:
+            result = match.group(1).strip()
+
+            add_license_badge()
+            add_custom_badge("branch", "branch", branch, "blue")
+
+            # Update the file content with the new badge. Replace result with the new badge list. join the list and add spaces
+            file_content = file_content.replace(result, " ".join(badge_list))
+
+            # Write the file
+            with open(file_read_me, 'w') as write_file:
+                write_file.write(file_content)
+                write_file.close()
+        else:
+            print("Error: Failed to find badge section")
+except:
+    print("Error: Could not read file")
+
+    # Exit the script
+    exit()
