553b085294e7460203cebaa086dac619e6155f35
Sven Scharmentke <sven@vastlimits.com>
2023-09-27 17:37:09 +0200

    Merge develop -> 7.1 (#154)
    Co-authored-by: vastlimits <github.action@localhost.local>
    Co-authored-by: vl-svc <vl-svc@users.noreply.github.com>
    Co-authored-by: Philipp <philipp.brzosa@gmx.de>
    Co-authored-by: svnscha <svnscha@users.noreply.github.com>
    Co-authored-by: PhBrz <PhBrz@users.noreply.github.com>
    Co-authored-by: Dieter Schmitz <dieter@uberagent.com>
    Co-authored-by: DieterSchmitz <DieterSchmitz@users.noreply.github.com>
    Co-authored-by: KRJulian <KRJulian@users.noreply.github.com>

--- config-dev/Security inventory/Windows/Shared/StlParser.ps1
+++ config-dev/Security inventory/Windows/Shared/StlParser.ps1
@@ -235,7 +235,7 @@ else {
         Add-Type -TypeDefinition $definitionCode -Language CSharp;
     }
     catch {
-        Write-Error "Failed to load StlParser: " + $_.Exception.Message;
+        Write-Error -Message "Failed to load StlParser: $($_.Exception.Message)"
     }
 }
 
@@ -248,7 +248,7 @@ function Get-vlCertificateTrustListFromBytes {
 
     #Check if byte array is not null
     if ($bytes.Length -eq 0) {
-        Write-Error "Invalid byte array (empty)"
+        Write-Error -Message "Invalid byte array (empty)"
         return
     }
 
@@ -258,7 +258,7 @@ function Get-vlCertificateTrustListFromBytes {
         $listOfTrustedCerts = [StlParser]::parseMemory($bytes);
     }
     catch {
-        Write-Error "Error while parsing file CTL: " + $_.Exception.Message
+        Write-Error -Message "Error while parsing file CTL: $($_.Exception.Message)"
         return
     }
 
