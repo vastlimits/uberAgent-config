532db0a343595c3710e43a26b32f521a122162c2
Philipp <philipp.brzosa@gmx.de>
2024-02-01 12:34:01 +0100

    PR Feedback

--- config-dev/Security inventory/macOS/Examples/Template.zsh
+++ config-dev/Security inventory/macOS/Examples/Template.zsh
@@ -1,10 +1,10 @@
 
 vlSimpleExample()
 {
-   # Please always use this block constisting of testName, testDisplayName and testDescription.
-   # Important for the piepline, these values are parsed and displayed on the dashboard.
+   # Please always use this block, which consists of testName, testDisplayName, and testDescription.
+   # Important for the pipeline, these values are parsed and displayed on the dashboard.
    testName="vlSimpleExample" # give the test a unique name
-   testDisplayName="Simple example" # give the test a human-readable name, this will be displayed at the dashboard
+   testDisplayName="Simple example" # give the test a human-readable name, this will be displayed on the dashboard
    testDescription="This test returns a simple result." # give the test a description
 
    testScore=10 # define the score for this test. Score ranges from 0 to 10 (10 is the highest score).
@@ -14,9 +14,17 @@ vlSimpleExample()
    result="true"
 
    # Add the result of your logic to the final output using vlAddResultValue
+
    # The parameters for vlAddResultValue are: $resultData, $key, $value
-   #  $resultData should be passed from call to call and contains the result object, use "{}" to create a new result object or "[]" to create a new result array
-   #  $value is the value you want to add
+   #
+   #  $resultData:
+   #     Should be passed from call to call and contains the result object, use "{}" to create a new result object or "[]" to create a new result array.
+   #     In this case, we use an object and add the Enabled property. Please see vlSimpleArrayExample for an array example.
+   #  $key:
+   #     The key under which the value should be added. Leave empty if you want to add a value to an array. Please see vlSimpleArrayExample for an array example.
+   #  $value:
+   #     Is the value you want to add. In this case, we want to add the value of $result.
+
    resultData=$(vlAddResultValue "{}" "Enabled" $result)
 
    # Create the result object
@@ -25,10 +33,10 @@ vlSimpleExample()
 
 vlSimpleArrayExample()
 {
-   # Arrays can be used for tests. It is important to note that the dashboard currrently does only support arrays as a top-level object.
+   # Arrays can be used for tests. It is important to note that the dashboard currently does only support arrays as a top-level object.
 
-   # Please always use this block constisting of testName, testDisplayName and testDescription.
-   # Important for the piepline, these values are parsed and displayed on the dashboard.
+   # Please always use this block, which consists of testName, testDisplayName, and testDescription.
+   # Important for the pipeline, these values are parsed and displayed on the dashboard.
    testName="vlSimpleArrayExample" # give the test a unique name
    testDisplayName="Simple array example" # give the test a human-readable name, this will be displayed at the dashboard
    testDescription="This test returns a simple result array." # give the test a description
@@ -44,9 +52,16 @@ vlSimpleArrayExample()
    resultObj2=$(vlAddResultValue "$resultObj2" "Age" "43")
 
    # Add the result of your logic to the final output using vlAddResultValue
+
    # The parameters for vlAddResultValue are: $resultData, $key, $value
-   #  $resultData should be passed from call to call and contains the result object, use "{}" to create a new result object or "[]" to create a new result array
-   #  $value is the value you want to add
+   #
+   #  $resultData:
+   #     Should be passed from call to call and contains the result object, use "{}" to create a new result object or "[]" to create a new result array.
+   #     In this case, we add two objects to the result array. Please see vlSimpleExample or vlNestedExample for an object example.
+   #  $key:
+   #     The key under which the value should be added. Leave empty if you want to add a value to an array.
+   #  $value:
+   #     The value you want to add. In this case, we want to add the two objects we created above.
 
    # Option 1 all at once. Use "[]" to create a new array, since there is no key within an array leave key empty.
    resultData=$(vlAddResultValue "[]" "" "[$resultObj1, $resultObj2]")
@@ -61,8 +76,8 @@ vlSimpleArrayExample()
 
 vlNestedExample()
 {
-   # Please always use this block constisting of testName, testDisplayName and testDescription.
-   # The pipeline will try to parse these values and display them in the dashboard.
+   # Please always use this block, which consists of testName, testDisplayName, and testDescription.
+   # Important for the pipeline, these values are parsed and displayed on the dashboard.
    testName="vlNestedExample" # give the test a unique name
    testDisplayName="Nested result" # give the test a human-readable name, this will be displayed at the dashboard
    testDescription="This test returns a nested result." # give the test a description
@@ -81,7 +96,7 @@ vlNestedExample()
    resultData=$(vlAddResultValue "$resultData" "Person.Name" "John")
    resultData=$(vlAddResultValue "$resultData" "Person.Age" 30)
 
-   # While it is technically possible to add arrays to an object, the dashboard cannot display them correctly, so please avoid doing so.
+   # While it is technically possible to add arrays to an nested object, the dashboard cannot display them correctly, so please avoid doing so.
    # resultData=$(vlAddResultValue "$resultData" "Person.Colors" '["blue"]')
    # resultData=$(vlAddResultValue "$resultData" "Person.Colors" '["red","yellow"]')
 
