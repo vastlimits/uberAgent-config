fac72b84175530ae2c7c3ee3298e1427bba2b7e4
PhBrz <PhBrz@users.noreply.github.com>
2024-02-01 08:19:38 +0000

    Updated transpiled output

--- config/Security inventory/macOS/Firewall/Firewall.zsh
+++ config/Security inventory/macOS/Firewall/Firewall.zsh
@@ -73,7 +73,8 @@ vlGetFirewallApprovedApps()
     return
   fi
 
-  local approvedApps=()
+  local resultData=$(vlAddResultValue "" "ApprovedApplications" '[]')
+
   printf "$vlCommandStdout" | \
     grep -B1 '( Allow incoming connections )' | \
     awk '/^[0-9]* *:/' | \
@@ -81,17 +82,10 @@ vlGetFirewallApprovedApps()
     awk '{$1=$1};1' | \
   while IFS= read -r appPath
   do
-    approvedApps+=$( "$JQ" $JQFLAGS -n --arg appPath "$appPath" '$appPath' )
+    resultData=$(vlAddResultValue "$resultData" "ApprovedApplications" "[\"$appPath\"]")
   done
 
-  vlReportTestResultJsonResultDataArray \
-    "$testName" \
-    "$testDisplayName" \
-    "$testDescription" \
-    "$testScore" \
-    "$riskScore" \
-    "ApprovedApplications" \
-    ${approvedApps[@]}
+  vlCreateResultObject "$testName" "$testDisplayName" "$testDescription" "$testScore" "$riskScore" "$resultData"
 }
 
 ################################################################################
@@ -99,7 +93,7 @@ vlGetFirewallApprovedApps()
 ################################################################################
 
 # Initialize the vl* utility functions
-vlUtils="$( realpath "$( dirname $0 )/.." )/Utils.zsh"
+vlUtils="$(cd "$(dirname "$0")/.." && pwd)/Utils.zsh"
 . "$vlUtils" && vlInit
 
 # Run the tests
