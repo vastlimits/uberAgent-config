556bf069c11227dcc310c8810b679db0b6e2b49d
Federico Sauter <federico@vastlimits.com>
2024-04-22 12:45:07 +0200

    Ensure that the DN is displayed with the same format as Windows does

--- config-dev/Security inventory/macOS/Certificate/Certificate.zsh
+++ config-dev/Security inventory/macOS/Certificate/Certificate.zsh
@@ -11,7 +11,7 @@ vlReportTrustStoreVersion()
   local riskScore=0
   local testScore=10
 
-  vlRunCommand defaults read /System/Library/Security/Certificates.bundle/Contents/version.plist CFBundleVersion
+  vlRunCommand /usr/bin/defaults read /System/Library/Security/Certificates.bundle/Contents/version.plist CFBundleVersion
 
   if (( $vlCommandStatus != 0 )); then
     vlReportErrorJson \
@@ -45,11 +45,11 @@ vlCheckSystemKeychainCerts()
   keychainCheckResult="[]"
   certTrustMap=()
 
-  vlBuildCertTrustMap "$( security dump-trust-settings -d 2>/dev/null )"
+  vlBuildCertTrustMap "$( /usr/bin/security dump-trust-settings -d 2>/dev/null )"
   # Ignore the self-signed machine certificates installed by macOS
   vlCheckKeychains \
     "" \
-    "$( security list-keychains -d system)" \
+    "$( /usr/bin/security list-keychains -d system)" \
     com.apple.kerberos.kdc com.apple.systemdefault
 
   local testScore=10
@@ -84,10 +84,10 @@ vlCheckUserKeychains()
     if [[ -z "$pwdPolicy" ]] \
       || [[ "$pwdPolicy" = "newPasswordRequired=0" ]]
     then
-      vlBuildCertTrustMap "$(su -l $username -c 'security dump-trust-settings 2>/dev/null')"
+      vlBuildCertTrustMap "$(/usr/bin/su -l $username -c '/usr/bin/security dump-trust-settings 2>/dev/null')"
       vlCheckKeychains \
         "$username" \
-        "$( su -l $username -c 'security list-keychains -d user' )"
+        "$( /usr/bin/su -l $username -c '/usr/bin/security list-keychains -d user' )"
     fi
   done
 
@@ -182,7 +182,7 @@ vlIsCertTrustRootForAllUsers()
   trap 'rm -f "'"$certFile"'"' EXIT
 
   echo "$singleCertData" >$certFile
-  security verify-cert -c $certFile >/dev/null 2>&1
+  /usr/bin/security verify-cert -c $certFile >/dev/null 2>&1
   local verifyCertStatus=$?
   rm -f "$certFile"
 
@@ -194,13 +194,13 @@ vlIsRootCertificate()
   local singleCertData="$1"
 
   # Check if the certificate has the CA basic constraint set or not.
-  if [[ -z "$( echo "$singleCertData" | openssl x509 -noout -text 2>/dev/null | grep 'CA:TRUE' )" ]]; then
+  if [[ -z "$( echo "$singleCertData" | /usr/bin/openssl x509 -noout -text 2>/dev/null | grep 'CA:TRUE' )" ]]; then
     return 1
   fi
 
   # Check if the certificate is self-signed, i.e. the root CA certificate.
-  local issuer="$( echo "$singleCertData" | openssl x509 -noout -issuer 2>/dev/null | sed 's/issuer=//' )"
-  local subject="$( echo "$singleCertData" | openssl x509 -noout -subject 2>/dev/null | sed 's/subject=//' )"
+  local issuer="$( echo "$singleCertData" | /usr/bin/openssl x509 -noout -issuer 2>/dev/null | sed 's/issuer=//' )"
+  local subject="$( echo "$singleCertData" | /usr/bin/openssl x509 -noout -subject 2>/dev/null | sed 's/subject=//' )"
   if [ "$issuer" != "$subject" ]; then
     return 1
   fi
@@ -222,6 +222,57 @@ vlShouldIgnoreCertificate()
   return 1
 }
 
+vlUnescapeCommas()
+{
+  sed 's/\\,/,/g; s/\\\\,/,/g'
+}
+
+# Convert the RFC2253 CN fields from LibreSSL:
+#
+#   'subject= CN=ad-DC1-CA,DC=ad,DC=int,DC=vastlimits,DC=com'
+#
+# to the certificate summary that Apple uses to identify the trust
+# settings (and display in the UI):
+#
+#   'ad-DC1-CA'
+vlNormalizeDnSummary()
+{
+  perl -ne 'if(/CN=((?:[^,\\]|\\.)*)/){print "$1\n"; exit}' | vlUnescapeCommas
+}
+
+# Convert the RFC2253 CN fields from LibreSSL:
+#
+#   'subject= CN=ad-DC1-CA,DC=ad,DC=int,DC=vastlimits,DC=com'
+#
+# to comma-separated names the Windows implementation uses:
+#
+#   'CN=ad-DC1-CA, DC=ad, DC=int, DC=vastlimits, DC=com'
+vlNormalizeDn()
+{
+  awk -F'[,/]' '{
+    output = "";
+    firstFieldFound = 0;
+    for (i = 1; i <= NF; i++) {
+      gsub(/^ +| +$/, "", $i);
+      if (length($i) > 0) {
+        if (firstFieldFound) {
+          output = output ", " $i;
+        } else {
+          output = $i;
+          firstFieldFound = 1;
+        }
+      }
+    }
+    print output;
+  }' | vlUnescapeCommas
+}
+
+# Use the same date format that Windows uses.
+vlNormalizeDate()
+{
+  xargs -I{} date -j -f "%b %d %T %Y %Z" "{}" "+%Y-%m-%dT%H:%M:%S"
+}
+
 # Check that the specified keychain doesn't have any trusted root CA certificates
 # which are presumed to have been added by the user. Ignore the certificates present
 # in the ignored CN function parameter(s).
@@ -238,7 +289,7 @@ vlCheckKeychains()
     local keychainPath=$( echo "$keychainRaw" | xargs )
 
     IFS='|'
-    for singleCertData in $( security find-certificate -a -p "$keychainPath" | \
+    for singleCertData in $( /usr/bin/security find-certificate -a -p "$keychainPath" |
       awk -v RS='-----END CERTIFICATE-----' \
       'NF{printf "%s|", $0 "-----END CERTIFICATE-----"}' )
     do
@@ -247,11 +298,9 @@ vlCheckKeychains()
       # The different fields in the subject are separated by commas. If the CN itself
       # contains commas, then they are escaped with the backslash character.
       local commonName=$(
-          echo "$singleCertData" | \
-          openssl x509 -noout -subject -nameopt RFC2253 2>/dev/null | \
-          perl -ne 'if(/CN=((?:[^,\\]|\\.)*)/){print "$1\n"; exit}' | \
-          sed 's/\\,/,/g; s/\\\\,/,/g'
-      )
+          echo "$singleCertData" |
+          /usr/bin/openssl x509 -noout -subject -nameopt RFC2253 2>/dev/null |
+          vlNormalizeDnSummary )
 
       vlShouldIgnoreCertificate "$commonName" $@ && continue
 
@@ -262,25 +311,27 @@ vlCheckKeychains()
         || continue
 
       local issuer=$(echo "$singleCertData" |
-                    openssl x509 -noout -issuer 2>/dev/null |
-                    sed 's/issuer=//')
-
-      local notBefore=$(echo "$singleCertData" |
-                        openssl x509 -noout -startdate 2>/dev/null |
-                        cut -d= -f2 |
-                        xargs -I{} date -j -f "%b %d %T %Y %Z" "{}" "+%Y-%m-%dT%H:%M:%S")
-
-      local notAfter=$(echo "$singleCertData" |
-                      openssl x509 -noout -enddate 2>/dev/null |
-                      cut -d= -f2 |
-                      xargs -I{} date -j -f "%b %d %T %Y %Z" "{}" "+%Y-%m-%dT%H:%M:%S")
+                    /usr/bin/openssl x509 -noout -issuer -nameopt RFC2253 2>/dev/null |
+                    sed 's/issuer=//' |
+                    vlNormalizeDn)
 
       local subject=$(echo "$singleCertData" |
-                      openssl x509 -noout -subject 2>/dev/null |
-                      sed 's/subject=//')
+                      /usr/bin/openssl x509 -noout -subject -nameopt RFC2253 2>/dev/null |
+                      sed 's/subject=//' |
+                      vlNormalizeDn)
+
+      local notBefore=$(echo "$singleCertData" |
+                        /usr/bin/openssl x509 -noout -startdate 2>/dev/null |
+                        cut -d= -f2 |
+                        vlNormalizeDate)
+
+      local notAfter=$(echo "$singleCertData" |
+                      /usr/bin/openssl x509 -noout -enddate 2>/dev/null |
+                      cut -d= -f2 |
+                      vlNormalizeDate)
 
       local thumbprint=$(echo "$singleCertData" |
-                        openssl x509 -noout -sha1 -fingerprint 2>/dev/null |
+                        /usr/bin/openssl x509 -noout -sha1 -fingerprint 2>/dev/null |
                         tr -d ':' |
                         cut -d= -f2)
 
