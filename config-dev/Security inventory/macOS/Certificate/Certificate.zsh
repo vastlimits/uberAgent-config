6c9c405ef0b07b6306755e8876d2b90b8e576d29
Fred <150280979+vl-federico@users.noreply.github.com>
2024-04-25 09:36:27 +0200

    macOS: Adapt SCI Certificate Tests Names and ResultData to match Windows (#222)

--- config-dev/Security inventory/macOS/Certificate/Certificate.zsh
+++ config-dev/Security inventory/macOS/Certificate/Certificate.zsh
@@ -11,7 +11,7 @@ vlReportTrustStoreVersion()
   local riskScore=0
   local testScore=10
 
-  vlRunCommand defaults read /System/Library/Security/Certificates.bundle/Contents/version.plist CFBundleVersion
+  vlRunCommand /usr/bin/defaults read /System/Library/Security/Certificates.bundle/Contents/version.plist CFBundleVersion
 
   if (( $vlCommandStatus != 0 )); then
     vlReportErrorJson \
@@ -36,18 +36,21 @@ vlReportTrustStoreVersion()
 
 vlCheckSystemKeychainCerts()
 {
-  local testName="CertSysKeyChk"
-  local testDisplayName="System Keychain Cert Check"
+  local testName="CMTrByMac"
+  local testDisplayName="Certificates trusted by macOS - Machine"
   local testDescription="This test examines the system keychain for any trusted root CA certificates added by an admin. These certificates, if present, could alter the default trust settings of the system and potentially introduce security risks. The absence of admin-added certificates is considered a good state, while their presence requires further investigation to ensure system integrity."
-  local riskScore=5
+  local riskScore=70
 
   keychainCheckCertCount=0
   keychainCheckResult="[]"
   certTrustMap=()
 
-  vlBuildCertTrustMap "$( security dump-trust-settings -d 2>/dev/null )"
+  vlBuildCertTrustMap "$( /usr/bin/security dump-trust-settings -d 2>/dev/null )"
   # Ignore the self-signed machine certificates installed by macOS
-  vlCheckKeychains "" "$( security list-keychains -d system)" com.apple.kerberos.kdc com.apple.systemdefault
+  vlCheckKeychains \
+    "" \
+    "$( /usr/bin/security list-keychains -d system)" \
+    com.apple.kerberos.kdc com.apple.systemdefault
 
   local testScore=10
   if (( keychainCheckCertCount > 0 )); then
@@ -65,10 +68,10 @@ vlCheckSystemKeychainCerts()
 
 vlCheckUserKeychains()
 {
-  local testName="CertUserKeyChk"
-  local testDisplayName="User Keychain Cert Check"
+  local testName="CUTrByMac"
+  local testDisplayName="Certificates trusted by macOS - User"
   local testDescription="This test checks all user keychains for any added trusted root CA certificates. Similar to the system keychain, user-added certificates in their keychains could affect the system's trust settings on a per-user basis. The absence of user-added certificates is considered a good state, while their presence could indicate potential security concerns that need to be addressed."
-  local riskScore=5
+  local riskScore=70
 
   keychainCheckCertCount=0
   keychainCheckResult="[]"
@@ -80,8 +83,10 @@ vlCheckUserKeychains()
     if [[ -z "$pwdPolicy" ]] \
       || [[ "$pwdPolicy" = "newPasswordRequired=0" ]]
     then
-      vlBuildCertTrustMap "$(su -l $username -c 'security dump-trust-settings 2>/dev/null')"
-      vlCheckKeychains "$username" "$( su -l $username -c 'security list-keychains -d user' )"
+      vlBuildCertTrustMap "$(/usr/bin/su -l $username -c '/usr/bin/security dump-trust-settings 2>/dev/null')"
+      vlCheckKeychains \
+        "$username" \
+        "$( /usr/bin/su -l $username -c '/usr/bin/security list-keychains -d user' )"
     fi
   done
 
@@ -175,7 +180,7 @@ vlIsCertTrustRootForAllUsers()
   trap 'rm -f "'"$certFile"'"' EXIT
 
   echo "$singleCertData" >$certFile
-  security verify-cert -c $certFile >/dev/null 2>&1
+  /usr/bin/security verify-cert -c $certFile >/dev/null 2>&1
   local verifyCertStatus=$?
   rm -f "$certFile"
 
@@ -187,13 +192,13 @@ vlIsRootCertificate()
   local singleCertData="$1"
 
   # Check if the certificate has the CA basic constraint set or not.
-  if [[ -z "$( echo "$singleCertData" | openssl x509 -noout -text 2>/dev/null | grep 'CA:TRUE' )" ]]; then
+  if [[ -z "$( echo "$singleCertData" | /usr/bin/openssl x509 -noout -text 2>/dev/null | grep 'CA:TRUE' )" ]]; then
     return 1
   fi
 
   # Check if the certificate is self-signed, i.e. the root CA certificate.
-  local issuer="$( echo "$singleCertData" | openssl x509 -noout -issuer 2>/dev/null | sed 's/issuer=//' )"
-  local subject="$( echo "$singleCertData" | openssl x509 -noout -subject 2>/dev/null | sed 's/subject=//' )"
+  local issuer="$( echo "$singleCertData" | /usr/bin/openssl x509 -noout -issuer 2>/dev/null | sed 's/issuer=//' )"
+  local subject="$( echo "$singleCertData" | /usr/bin/openssl x509 -noout -subject 2>/dev/null | sed 's/subject=//' )"
   if [ "$issuer" != "$subject" ]; then
     return 1
   fi
@@ -215,6 +220,57 @@ vlShouldIgnoreCertificate()
   return 1
 }
 
+vlUnescapeCommas()
+{
+  sed 's/\\,/,/g; s/\\\\,/,/g'
+}
+
+# Convert the RFC2253 CN fields from LibreSSL:
+#
+#   'subject= CN=my-CA,DC=ad,DC=mycompany,DC=com'
+#
+# to the certificate summary that Apple uses to identify the trust
+# settings (and display in the UI):
+#
+#   'my-CA'
+vlNormalizeDnSummary()
+{
+  perl -ne 'if(/CN=((?:[^,\\]|\\.)*)/){print "$1\n"; exit}' | vlUnescapeCommas
+}
+
+# Convert the RFC2253 CN fields from LibreSSL:
+#
+#   'subject= CN=my-CA,DC=ad,DC=mycompany,DC=com'
+#
+# to comma-separated names the Windows implementation uses:
+#
+#   'CN=my-CA, DC=ad, DC=mycompany, DC=com'
+vlNormalizeDn()
+{
+  awk -F'[,/]' '{
+    output = "";
+    firstFieldFound = 0;
+    for (i = 1; i <= NF; i++) {
+      gsub(/^ +| +$/, "", $i);
+      if (length($i) > 0) {
+        if (firstFieldFound) {
+          output = output ", " $i;
+        } else {
+          output = $i;
+          firstFieldFound = 1;
+        }
+      }
+    }
+    print output;
+  }' | vlUnescapeCommas
+}
+
+# Use the same date format that Windows uses.
+vlNormalizeDate()
+{
+  xargs -I{} date -j -f "%b %d %T %Y %Z" "{}" "+%Y-%m-%dT%H:%M:%S"
+}
+
 # Check that the specified keychain doesn't have any trusted root CA certificates
 # which are presumed to have been added by the user. Ignore the certificates present
 # in the ignored CN function parameter(s).
@@ -223,6 +279,7 @@ vlCheckKeychains()
 {
   local username="$1"
   local keychains="$2"
+
   shift 2
 
   echo "$keychains" | while IFS= read -r keychainRaw
@@ -230,7 +287,7 @@ vlCheckKeychains()
     local keychainPath=$( echo "$keychainRaw" | xargs )
 
     IFS='|'
-    for singleCertData in $( security find-certificate -a -p "$keychainPath" | \
+    for singleCertData in $( /usr/bin/security find-certificate -a -p "$keychainPath" |
       awk -v RS='-----END CERTIFICATE-----' \
       'NF{printf "%s|", $0 "-----END CERTIFICATE-----"}' )
     do
@@ -239,11 +296,9 @@ vlCheckKeychains()
       # The different fields in the subject are separated by commas. If the CN itself
       # contains commas, then they are escaped with the backslash character.
       local commonName=$(
-          echo "$singleCertData" | \
-          openssl x509 -noout -subject -nameopt RFC2253 2>/dev/null | \
-          perl -ne 'if(/CN=((?:[^,\\]|\\.)*)/){print "$1\n"; exit}' | \
-          sed 's/\\,/,/g; s/\\\\,/,/g'
-      )
+          echo "$singleCertData" |
+          /usr/bin/openssl x509 -noout -subject -nameopt RFC2253 2>/dev/null |
+          vlNormalizeDnSummary )
 
       vlShouldIgnoreCertificate "$commonName" $@ && continue
 
@@ -253,10 +308,41 @@ vlCheckKeychains()
         || vlIsCertTrustRootForAllUsers "$singleCertData" \
         || continue
 
-      local certObj=$( vlAddResultValue "{}" "CommonName" "$commonName" )
+      local issuer=$(echo "$singleCertData" |
+                    /usr/bin/openssl x509 -noout -issuer -nameopt RFC2253 2>/dev/null |
+                    sed 's/issuer=//' |
+                    vlNormalizeDn)
+
+      local subject=$(echo "$singleCertData" |
+                      /usr/bin/openssl x509 -noout -subject -nameopt RFC2253 2>/dev/null |
+                      sed 's/subject=//' |
+                      vlNormalizeDn)
+
+      local notBefore=$(echo "$singleCertData" |
+                        /usr/bin/openssl x509 -noout -startdate 2>/dev/null |
+                        cut -d= -f2 |
+                        vlNormalizeDate)
+
+      local notAfter=$(echo "$singleCertData" |
+                      /usr/bin/openssl x509 -noout -enddate 2>/dev/null |
+                      cut -d= -f2 |
+                      vlNormalizeDate)
+
+      local thumbprint=$(echo "$singleCertData" |
+                        /usr/bin/openssl x509 -noout -sha1 -fingerprint 2>/dev/null |
+                        tr -d ':' |
+                        cut -d= -f2)
+
+      local certObj="{}"
+      certObj=$( vlAddResultValue "${certObj}" "Issuer" "$issuer" )
+      certObj=$( vlAddResultValue "${certObj}" "NotAfter" "$notAfter" )
+      certObj=$( vlAddResultValue "${certObj}" "NotBefore" "$notBefore" )
+      certObj=$( vlAddResultValue "${certObj}" "Subject" "$subject" )
+      certObj=$( vlAddResultValue "${certObj}" "Thumbprint" "$thumbprint" )
+
       if [[ -n "$username" ]]; then
+        certObj=$( vlAddResultValue "${certObj}" "Username" "$username" )
         local keychainName=$( basename "$keychainPath" | cut -d"." -f1 | tr '[:upper:]' '[:lower:]' )
-        certObj=$( vlAddResultValue "${certObj}" "User" "$username" )
         certObj=$( vlAddResultValue "${certObj}" "Keychain" "$keychainName" )
       fi
 
@@ -264,6 +350,10 @@ vlCheckKeychains()
       keychainCheckCertCount=$(( keychainCheckCertCount + 1 ))
     done
   done
+
+  if [[ -n "$username" ]] && (( keychainCheckCertCount == 0 )); then
+    keychainCheckResult=$(vlAddResultValue "{}" "Username" "$username" )
+  fi
 }
 
 vlCertsTests()
